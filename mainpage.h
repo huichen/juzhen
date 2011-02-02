b294915612ca2ec470e5d88287b13058f5426a4c
Hui Chen <usa.chen@gmail.com>
2011-02-02 02:35:41 -0600

    improve mainpage.h

--- mainpage.h
+++ mainpage.h
@@ -1,4 +1,4 @@
-/** @mainpage package templates
+/** @mainpage Matrix Library for C++ (mlcpp) 
 *
 * @author Hui Chen "<usa dot chen at gmail>" 
 *
@@ -19,26 +19,29 @@
 * @section how_to_use How to use
   -# Set MLCPP_BLASLIB envron to mkl if you want to use MKL <br> <br> 
      export MLCPP_BLASLIB=mkl<br><br> 
-  -# Run unit tests first and make sure it passes everything:<br><br> 
-     cd mlcpp_source_dir<br><br> 
+  -# Run unit tests and make sure it passes everything:<br><br> 
+     cd mlcpp_source_dir <br> <br> 
      make <br><br> 
-  -# #include <mlcpp.hpp> in your source file <br><br> 
-     and add '-Imlcpp_source_dir' to your compile flags. <br><br> 
-     Use '-DUSE_MKL' if you use MKL <br><br> 
+  -# Include mlcpp.hpp in your source file and add '-Imlcpp_source_dir' to your compiler flags. <br><br> 
+     Use '-DUSE_MKL' if you build with MKL <br><br> 
+
+* @section example Examples
+  
+  There are plenty samples in unittest.cpp file. Reading comments in the code helps too.
 
 * @section notes Notes
 
-  -# Following functions only compiles with MKL <br><br> 
+  -# Following functions only compile with MKL <br><br> 
      matrix::eigen, matrix::leigen, matrix::reigen <br><br> 
-  -# matrix::getDataPtr() provides column-major raw pointer to be used in low-level BLAS/LAPACK functions.<br><br> 
+  -# matrix::getDataPtr() provides column-major raw pointer for low-level BLAS/LAPACK functions.<br><br> 
 
 *  @section problems_and_improvements Problems and improvements
 
-  Please report issues through github:
+  Send me an email or report issues through github:
 
   https://github.com/huichen/mlcpp/issues
 
-  I don't have time to write all new features you want but I'm very glad to merge them into my branch.
+  I don't have time to write the new features that you want but I'm very glad to merge them into my branch.
 
 *
 */
