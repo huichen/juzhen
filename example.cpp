d717adb3950efe546c0bdd766e55f45d82cbb6e6
Hui Chen <usa.chen@gmail.com>
2011-02-09 11:40:13 -0600

    fix example

--- example.cpp
+++ example.cpp
@@ -25,12 +25,10 @@
 using namespace mlcpp;
 
 int main() {
-  dvector potential(N);
+  cmatrix H(N, N);
   for (int i=0; i<N; i++) 
-    if (i<N/3 || i>=2*N/3) potential(i) = 1000;
-    else potential(i) = 0;
-
-  cmatrix H = diag(potential);
+    if (i<N/3 || i>=2*N/3) H(i, i) = 1000;
+    else H(i, i) = 0;
   for (int i=0; i<N-1; i++) H(i, i+1) = H(i+1, i) = -1;
 
 #ifdef USE_MKL
