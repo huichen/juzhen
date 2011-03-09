321dce160c7d64b0ed2332a3db344d34c78915e4
Hui Chen <usa.chen@gmail.com>
2011-03-09 12:29:33 -0600

    [adaptor] separate gemm

--- src/adaptor/native/gemm.h
+++ src/adaptor/native/gemm.h
@@ -20,49 +20,30 @@
 +---------------------------------------------------------------------------+
 */
 
-#ifndef SRC_ADAPTOR_NATIVE_H_
-#define SRC_ADAPTOR_NATIVE_H_
-#include <omp.h>
-
-#include <assert.h>
-
-#include <adaptor/native/gemm.h>
+#ifndef SRC_ADAPTOR_NATIVE_GEMM_H_
+#define SRC_ADAPTOR_NATIVE_GEMM_H_
 
 namespace mlcpp {
-
 template<typename T>
-int geev(
-    char nl, char nr, const int n,
-    T *a, const int lda, CD * w, T *vl, const int ldvl,
-    T *vr, const int ldvr) {
-  assert(0);  // always fails
-}
-
-/*
- * Linear solver
- */
-template<typename T>
-int gesv(
-    const int n, const int nrhs,
-    T *a, const int lda, T *b, const int ldb) {
-  assert(0);  // always fails
-}
-
-/*
- * Matrix inversion 
- */
-template<typename T>
-int matrix_inverse(
-    const int m, const int n, T *a, const int lda) {
-  assert(0);  // always fails
-}
-
-/*
- * Matrix determinant 
- */
-template<typename T>
-T matrix_determinant(const int m, T *a) {
-  assert(0);  // always fails
+void gemm(
+    const int M, const int N,
+    const int K, const T *A, const int lda, const T *B,
+    const int ldb, T *c, const int ldc) {
+  const T *ai, *bi;
+  T *ci;
+  #pragma omp parallel for private(ai, bi, ci)
+  for (size_t i = 0; i < M; i++) {
+    for (size_t j = 0; j < N; j++) {
+      ai = A + i;
+      bi = B + j * ldb;
+      ci = c + j * ldc + i;
+      (*ci) = 0;
+      for (size_t k = 0; k < K; k++) {
+        (*ci) += (*ai) * (*(bi++));
+        ai += lda;
+      }
+    }
+  }
 }
 }
-#endif  // SRC_ADAPTOR_NATIVE_H_
+#endif  // SRC_ADAPTOR_NATIVE_GEMM_H_
